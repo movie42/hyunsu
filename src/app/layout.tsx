0dda49a63949bf9b48badedde2d0889be16432d7
HYUNSU <movie3342@gmail.com>
2024-07-20 13:02:48 +0900

    feat: favicon 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,9 @@ export const metadata: Metadata = {
     default: "현수의 블로그",
     template: `%s | 현수의 블로그`
   },
+  icons: {
+    icon: "/favicon.png"
+  },
   description: "프론트엔드 개발자 고현수의 블로그입니다. 개발, 자바스크립 & 잡다한 이야기를 끄적거리는 곳입니다.",
   verification: {
     google: "veIyQnpMzmYGRpL8GEjeQsflYmq9zJAVRpn5zVx16FY"
